2f6519f6e6e73a76790c8a070eaf4c8364071743
madar1992 <96854154+madar1992@users.noreply.github.com>
2023-01-25 11:47:36 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,73 +1,59 @@
-import express from 'express'
-import * as dotenv from 'dotenv'
-import cors from 'cors'
-import { Configuration, OpenAIApi } from 'openai'
+var express = require("express");
+var app = express();
+var PORT = 8080;
 
-dotenv.config()
+app.get('/:operator/:num1/:num2', function(req,res){
+	
+	var operator = req.params.operator
+	var num1 = parseInt(req.params.num1);
+	var num2 = parseInt(req.params.num2);
+	var result;
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+	switch(operator){
+		case "addition":
+		result = add(num1, num2);
+		break;
+
+		case "subtraction":
+		result = subtract(num1, num2);
+		break;
+
+		case "multiplication":
+		result = multiply(num1, num2);
+		break;
+
+		case "division":
+		result = divide(num1, num2);
+		break;
+
+		default:
+		result = "Sorry, please enter a valid operator!"
+	}
+
+	function add(a, b){
+		return a + b
+	}
+
+	function subtract(a, b){
+		return a - b
+	}
+
+	function multiply(a, b){
+		return a * b
+	}
+
+	function divide(a, b){
+		return a / b
+	}
+
+	res.json(result);
 });
-/*
-const config = {
-  model: "text-davinci-003",
-  temperature: 0,
-  max_tokens: 3000,
-  top_p: 1,
-  frequency_penalty: 0.5,
-  presence_penalty: 0,
-}
-*/
-const config = {
-  model: "text-davinci-003",
-  temperature: process.env.TEMPERATURE,
-  max_tokens: process.env.MAX_TOKENS,
-  top_p: process.env.TOP_P,
-  frequency_penalty: process.env.FREQUENCY_PENALTY,
-  presence_penalty: process.env.PRESENCE_PENALTY
 
- 
+app.get("/", function(req, res){
+	res.send("hello! please type some math y'all")
+});
 
-
-
-}
-
-const openai = new OpenAIApi(configuration);
-
-const app = express()
-app.use(cors())
-app.use(express.json())
-
-app.get('/', async (req, res) => {
-  res.status(200).send({
-    message: 'Hello from CodeX!'
-  })
-})
-
-app.post('/', async (req, res) => {
-  try {
-    const prompt = req.body.prompt;
-
-    const response = await openai.createCompletion({
-      model: config.model,
-  prompt: `${prompt}`,
-
-  temperature: config.temperature,
-  max_tokens: config.max_tokens,
-  top_p: config.top_p,
-  frequency_penalty: config.frequency_penalty,
-  presence_penalty: config.presence_penalty
-    });
-
-    res.status(200).send({
-      bot: response.data.choices[0].text
-    });
-
-  } catch (error) {
-    console.error(error)
-    res.status(500).send(error || 'Something went wrong');
-  }
-})
-
-
-app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
+//listener
+app.listen(PORT, function(){
+	console.log("App listening on PORT:" + PORT);
+});
